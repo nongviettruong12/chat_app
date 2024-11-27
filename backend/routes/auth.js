ee664b877d8e806c3c35f965fb09d3be62b9dad3
nongviettruong12 <nongviettruong12@gmail.com>
2024-11-27 15:01:05 +0700

    "backend":"update auth routes"

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const db = require('../database/database');
+const User = require('../models/user');
 const router = express.Router()
 
 const SECRET_KEY =  'vaicapia'
@@ -10,26 +10,19 @@ router.post('/register', (req, res) =>{
     const { username, password } = req.body
     const hashedPassword = bcrypt.hashSync(password, 10)
 
-    db.run (
-      'INSERT INTO users ( username, password ) VALUES (?, ?)',
-      [username, hashedPassword],
-      (err) =>{
-        if (err) {
-          return res.status(400).json({ error: 'Username already exists' });
-        }
-        res.status(201).json({ message: 'User registered successfully' });
-      }
-    )
+    User.create(username, hashedPassword, (err) =>{
+      if (err) return res.status(400).json({ error:'username already exists'})
+        res.json({ message:'user register success' })
+    })
 })
 router.post('/login', (req, res) =>{
   const { username, password } = req.body
 
-  db.get('SELECT * FROM users WHERE username = ?', [username], (err, user) => {
-    if (err ||!user) return res.status(404).json({ error: 'User not found' });
+  User.findByUsername(username, (err, user)=>{
+    if (err || !user) return res.status(404).json({ error:'user not found' })
 
-    const isValidPassword = bcrypt.compareSync(password, user.password)
-    if (!isValidPassword) return res.status(400).json({ error: 'Invalid password' });
-
+      const isValidPassword = bcrypt.compareSync(password, user.password)
+      if (!invalidPassword) return res.status(400).json({ error: 'invalid password'})
     const token = jwt.sign({ id: user.id}, SECRET_KEY, { expiresIn:'1h'})
     res.json({ token })
   })
